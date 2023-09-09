58f3ba7ad592a26c3bec3083dfd84c1164de06e9
Hekmon <edouardhur@gmail.com>
2023-09-09 16:21:01 +0200

    preparing v1.3.2

--- custom_components/rtetempo/const.py
+++ custom_components/rtetempo/const.py
@@ -52,7 +52,7 @@ API_VALUE_RED = "RED"
 API_VALUE_WHITE = "WHITE"
 API_VALUE_BLUE = "BLUE"
 API_ATTRIBUTION = "Données fournies par data.rte-france.com"
-USER_AGENT = "github.com/hekmon/rtetempo v1.3.1"
+USER_AGENT = "github.com/hekmon/rtetempo v1.3.2"
 
 
 # Tempo def
